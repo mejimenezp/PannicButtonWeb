34d94af25bd0c81f1e05bb9377e11aa3e11dcd75
mejimenezp <143832231+mejimenezp@users.noreply.github.com>
2025-03-22 12:07:53 -0500

    .

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,17 @@ const About = () => {
     <section className="about_section layout_padding">
       <div className="container">
         <div className="row">
-          {/* Texto y botón reorganizados */}
+          {/* Texto y botón */}
           <div className="col-lg-5 col-md-6">
             <div className="detail-box fade-in">
+              <Link to="/privacidad" className="btn">
+                Más Información
+              </Link>
               <h2>¿Qué es el Botón de Pánico?</h2>
               <p>
                 Nuestra aplicación te permite enviar alertas de emergencia con tu ubicación en tiempo real a tus contactos de confianza.  
                 Diseñada para brindarte seguridad en situaciones críticas.
               </p>
-              <Link to="/privacidad" className="btn">
-                Más Información
-              </Link>
             </div>
           </div>
 
