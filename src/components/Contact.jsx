a87c0a9a7ea5f6802d003b102685588f6bedee21
Miguel esteban Jimenez padilla <143832231+mejimenezp@users.noreply.github.com>
2025-05-15 13:15:11 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-
 const Contact = () => {
   return (
     <section className="contact">
       <div className="container">
         <h2>Contacto y Soporte</h2>
         <p>Si necesitas ayuda o tienes preguntas, contáctanos.</p>
+
+        {/* Información de contacto */}
+        <div className="contact-info">
+          <p>
+            <i className="fa fa-phone" aria-hidden="true"></i> Tel: +57 3185788810
+          </p>
+          <p>
+            <i className="fa fa-envelope" aria-hidden="true"></i> Email: <a href="mailto:maurviam@yahoo.com">maurviam@yahoo.com</a>
+          </p>
+        </div>
+
         <form>
           <input type="text" placeholder="Nombre" required />
           <input type="email" placeholder="Correo electrónico" required />
